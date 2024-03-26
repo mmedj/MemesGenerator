e3e89dcdf17807a468bede6c1bc19ac0914a28f8
mmedj <mohamedjaouhar1999@gmail.com>
2024-03-26 19:36:44 +0000

    Update index.jsx

--- src/components/Meme/index.jsx
+++ src/components/Meme/index.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { saveAs } from 'file-saver';
-import './Meme.css'; // Import the CSS file where classes are defined
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { saveAs } from "file-saver";
+import "./Meme.css"; // Import the CSS file where classes are defined
 
 const Meme = ({ meme, setMeme }) => {
   const { id } = useParams();
   const [memeData, setMemeData] = useState({
-    values: []
+    values: [],
   });
   const navigate = useNavigate();
 
-  let url = `https://api.imgflip.com/caption_image?template_id=${id}&username=Medj_1&password=Azerty12345`;
+  let url = `https://api.imgflip.com/caption_image?template_id=${id}&username=Yourusername_1&password=YourPass`;
 
   memeData.values.forEach((item, index) => {
     url += `&boxes[${index}][text]=${item.text}`;
@@ -20,12 +20,12 @@ const Meme = ({ meme, setMeme }) => {
     e.preventDefault();
 
     try {
-      const res = await fetch(url, { method: 'POST' });
+      const res = await fetch(url, { method: "POST" });
       const json = await res.json();
 
       setMeme({ ...meme, url: json.data.url });
     } catch (e) {
-      alert('Fill out the captions input');
+      alert("Fill out the captions input");
     }
   };
 
@@ -35,7 +35,7 @@ const Meme = ({ meme, setMeme }) => {
       newValues[index] = { text: e.target.value };
       setMemeData((prevState) => ({
         ...prevState,
-        values: newValues
+        values: newValues,
       }));
     };
 
@@ -56,11 +56,12 @@ const Meme = ({ meme, setMeme }) => {
 
   const backToHome = () => {
     setMeme(null);
-    navigate('/');
+    navigate("/");
   };
 
-  return (<>
-    <div className="container">
+  return (
+    <>
+      <div className="container">
         <h4>{meme.name}</h4>
         <div className="MemeWrapper">
           <img src={meme.url} className="Meme-img" alt="Meme" />
@@ -71,15 +72,19 @@ const Meme = ({ meme, setMeme }) => {
             <button type="button" className="Button" onClick={backToHome}>
               Get a new meme template
             </button>
-            <button type="submit"  className="Button" onClick={generateMeme}>
+            <button type="submit" className="Button" onClick={generateMeme}>
               Generate Meme
             </button>
-            <button type="submit"  className="Button"onClick={() => downloadImage(meme.url)}>
+            <button
+              type="submit"
+              className="Button"
+              onClick={() => downloadImage(meme.url)}
+            >
               Download Meme
             </button>
           </div>
         </form>
-    </div>
+      </div>
     </>
   );
 };
